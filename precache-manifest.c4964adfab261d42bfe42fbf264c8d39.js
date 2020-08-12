075718dff2f55a5e5e4e90b555654b56dc4afe82
Denis <rikaho@mail.ru>
2020-08-12 17:06:14 +0800

    Updates

---
+++ precache-manifest.c4964adfab261d42bfe42fbf264c8d39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8dd6213e9296224bbb31b02504d1e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0f69e3615163432d287",
+    "url": "./static/css/main.be7226cf.chunk.css"
+  },
+  {
+    "revision": "4e5e179af20408f97c8a",
+    "url": "./static/js/2.ab0215d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ab0215d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f69e3615163432d287",
+    "url": "./static/js/main.27571c89.chunk.js"
+  },
+  {
+    "revision": "b55990cac880bb5f7488",
+    "url": "./static/js/runtime-main.ac50c938.js"
+  },
+  {
+    "revision": "a1ffeacc013c2c0e633f76b8de2bd31e",
+    "url": "./static/media/gallery.a1ffeacc.png"
+  },
+  {
+    "revision": "e75b1e0e3e64fab71786b888ab4fdadf",
+    "url": "./static/media/notes.e75b1e0e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/portfolio-v1/precache-manifest.b9dbd9ce96fb2a4e2d69921f5b770edf.js"
+  "./precache-manifest.c4964adfab261d42bfe42fbf264c8d39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/portfolio-v1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
